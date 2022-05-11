5de196ec1be5e0bf69430daccbfa6320e55f8f6b
Dilovar <dilovar09@gmail.com>
2022-05-11 04:58:44 +0300

    ааа

--- app/Models/UnoProduct.php
+++ app/Models/UnoProduct.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class UnoProduct extends Model
 {
     use HasFactory;
+    protected $table = 'quke_products';
     protected $casts = [
         'pictures'   => 'json',
        // 'images'   => 'json',
